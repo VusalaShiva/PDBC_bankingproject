12ff34770426de8dc1b95b0d40b38121e13dcd40
VusalaShiva <vusalashivakumar@gmail.com>
2025-10-06 20:03:00 +0530

    Update db.py

--- Day1/db.py
+++ Day1/db.py
@@ -8,7 +8,7 @@ def get_connection():
     return mysql.connector.connect(
     host="localhost",
     user="root",
-    password="123456",
+    password="Mysql@143",
     database="EmployeeManagementSystem"
 )
 
